00467bb83b1a875284b2e8219faa80a0c3f2dba8
hsj1514 <45966091+hsj1514@users.noreply.github.com>
2019-01-14 19:55:59 +0900

    Add files via upload

---
+++ node.js/syntax/function.js
@@ -0,0 +1,22 @@
+f123();
+console.log('A');
+console.log('Z');
+console.log('B');
+f123();
+console.log('A');
+console.log('Z');
+console.log('B');
+console.log('F');
+f123();
+console.log('U');
+console.log('A');
+console.log('Z');
+console.log('J');
+console.log('I');
+f123();
+
+function f123(){
+  console.log(1);
+  console.log(2);
+  console.log(3);
+}
---
+++ node.js/syntax/function2.js
@@ -0,0 +1,10 @@
+console.log(Math.round(1.6));
+
+
+function sum(first, second){
+  return first+second; //return은 함수를 종료시킨다.
+}
+
+sum(2,4);
+
+Math.round(1.6);
